f9ac3ea373b6907fa1f24a10def11c448525bacc
Dexter <dexter.t.jones@outlook.com>
2024-04-20 21:56:03 -0600

    Added required code for Attributes

--- Fictional_Character/Attributes.cpp
+++ Fictional_Character/Attributes.cpp
@@ -1,5 +1,28 @@
+#include <iostream>
+#include <string>
 using namespace std;
 
 int main() {
+	string fName, lName, address, city, character;
+	int zipCode;
 
+	cout << "Please enter the first name of the fictional character: ";
+	getline(cin, fName);
+
+	cout << "Please enter the last name of the fictional character: ";
+	getline(cin, lName);
+
+	cout << "Please enter the street address of the fictional character: ";
+	getline(cin, address);
+
+	cout << "Please enter the city where the fictional character is from: ";
+	getline(cin, city);
+
+	cout << "Please enter the zip code where the fictional character is from: ";
+	cin >> zipCode;
+
+	character = fName + " " + lName + ", " + address + ", " + city + " " + to_string(zipCode);
+
+	cout << character;
+	cout << "Exiting... Goodbye";
 }
